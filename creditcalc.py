14675d0581321bbf5a1abe7b53a6e647202581a1
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-10-21 13:28:48 +0200

    Stage 2/4: Dreamworld

--- creditcalc.py
+++ creditcalc.py
@@ -1,12 +1,23 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-# write your code here
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+# Prompt user for principal and type of calculation
+principal = int(input('Enter the load principal: '))
+calc_type = input('What do you want to calculate?\ntype "m" - for the '
+                  'monthly payments,\ntype "p" for the monthly payment: ')
+
+# Calculate the number of monthly payments
+if calc_type == 'm':
+    monthly_payment = int(input('Enter the monthly payment: '))
+    months = math.ceil(principal / monthly_payment)
+    print(f'It will take {months} month to repay the loan' if months == 1
+          else f'It will take {months} months to repay the loan')
+
+# Calculate the amount of monthly payments
+if calc_type == 'p':
+    months = int(input('Enter the number of months: '))
+    monthly_payment = math.ceil(principal / months)
+    last_payment = principal - ((months - 1) * monthly_payment)
+    print(f'Your monthly payment = {monthly_payment} and the last payment = '
+          f'{last_payment}')
+
+
