c5b4bbef633d51dacf1b0529617947afde8a63fd
basvreeken <57438379+basvreeken@users.noreply.github.com>
2021-10-21 21:13:11 +0200

    Stage 3/4: Annuity payment

--- creditcalc.py
+++ creditcalc.py
@@ -1,23 +1,68 @@
 import math
 
-# Prompt user for principal and type of calculation
-principal = int(input('Enter the load principal: '))
-calc_type = input('What do you want to calculate?\ntype "m" - for the '
-                  'monthly payments,\ntype "p" for the monthly payment: ')
+# Prompt user for type of calculation
+calc_type = input('What do you want to calculate?\ntype "n" - for the '
+                  'monthly payments,\ntype "a" for annuity monthly payment '
+                  'amount,\ntype "p" for loan principal: ')
 
 # Calculate the number of monthly payments
-if calc_type == 'm':
+if calc_type == 'n':
+    principal = int(input('Enter the loan principal: '))
     monthly_payment = int(input('Enter the monthly payment: '))
-    months = math.ceil(principal / monthly_payment)
-    print(f'It will take {months} month to repay the loan' if months == 1
-          else f'It will take {months} months to repay the loan')
+    loan_interest = float(input('Enter the loan interest: '))
+    nominal_interest_rate = loan_interest / (12 * 100)
+    months = math.ceil(math.log(monthly_payment / (monthly_payment -
+                                                   nominal_interest_rate *
+                                                   principal),
+                                1 + nominal_interest_rate))
+
+    years = math.floor(months / 12)
+    rest_months = months % 12
+
+    # Only months
+    if years < 1:
+        print(f'It will take {rest_months} month to repay the loan' if
+              rest_months == 1
+              else f'It will take {rest_months} months to repay the loan')
+
+    # Only years
+    elif months % 12 == 0:
+        print(f'It will take {years} year to repay the loan' if years == 1
+              else f'It will take {years} years to repay the loan')
+
+    # 1 years and months
+    elif 12 < months < 24:
+        print(f'It will take {years} year and {rest_months} month to repay '
+              f'the loan' if rest_months == 1 else f'It will take {years} '
+                                                   f'years and {rest_months} '
+                                                   f'months to repay the loan')
+    else:
+        print(f'It will take {years} years and {rest_months} month to repay '
+              f'the loan' if rest_months == 1 else f'It will take {years} '
+                                                   f'years and {rest_months} '
+                                                   f'months to repay the loan')
+
+# Calculate the annuity monthly payment amount
+if calc_type == 'a':
+    principal = int(input('Enter the loan principal: '))
+    periods = int(input('Enter the number of periods: '))
+    loan_interest = float(input('Enter the loan interest: '))
+    nominal_interest_rate = loan_interest / (12 * 100)
+    factor = (nominal_interest_rate * math.pow((1 + nominal_interest_rate),
+                                               periods)) / \
+             (math.pow((1 + nominal_interest_rate), periods) - 1)
+    ordinary_annuity = math.ceil(principal * factor)
+    print(f'Your monthly payment = {ordinary_annuity}!')
 
 # Calculate the amount of monthly payments
 if calc_type == 'p':
-    months = int(input('Enter the number of months: '))
-    monthly_payment = math.ceil(principal / months)
-    last_payment = principal - ((months - 1) * monthly_payment)
-    print(f'Your monthly payment = {monthly_payment} and the last payment = '
-          f'{last_payment}')
+    annuity_payment = float(input('Enter the annuity payment'))
+    periods = int(input('Enter the number of periods: '))
+    loan_interest = float(input('Enter the loan interest: '))
+    nominal_interest_rate = loan_interest / (12 * 100)
+    factor = (nominal_interest_rate * math.pow((1 + nominal_interest_rate),
+                                               periods)) / \
+             (math.pow((1 + nominal_interest_rate), periods) - 1)
 
-
+    loan_principal = int(annuity_payment / factor)
+    print(f'Your loan principal = {loan_principal}!')
